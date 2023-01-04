ca5854bd9e209f0b308fe0b48386168d8b8374ca
Alexandre TOBIA <alexandre.tobia@gmail.com>
2023-01-04 12:27:35 +0100

    Initial commit

---
+++ resources/js/Pages/Blog/Index.vue
@@ -0,0 +1,29 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/inertia-vue3';
+
+defineProps({
+    posts: Array
+});
+</script>
+
+<template>
+    <Head title="Blog" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Blog</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">Articles : </div>
+                    <div v-for="post in posts">
+                        {{post.title}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
